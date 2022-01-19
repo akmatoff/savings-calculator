a33cc8633d6fda7e2e28427f29ff94f7160ac67f
akmatoff <akmatoff.exe@gmail.com>
2022-01-19 19:27:37 +0600

    Initial commit, IMPLEMENTED: Container, toggle switch, text

---
+++ src/components/Container.tsx
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+
+import ToggleSwitch from "./ToggleSwitch";
+
+export default function Container() {
+  return (
+    <StyledContainer>
+      <h1>
+        Savings <br /> calculator
+      </h1>
+      <div style={{ display: "flex" }}>
+        <ToggleSwitch />
+        <span>Calculate by total amount</span>
+      </div>
+    </StyledContainer>
+  );
+}
+
+const StyledContainer = styled.div`
+  width: 400px;
+  height: 606px;
+  padding: 43px 45px;
+  background: #fffefe;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.1), 0px 0px 20px rgba(0, 0, 0, 0.1);
+  border-radius: 6px;
+
+  h1 {
+    font-size: 30px;
+    font-weight: normal;
+    line-height: 37px;
+  }
+
+  span {
+    margin-left: 5px;
+    font-size: 14px;
+    line-height: 17px;
+    color: #102c51;
+  }
+`;
---
+++ src/components/ToggleSwitch.tsx
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+export default function ToggleSwitch() {
+  return (
+    <StyledToggleSwitch>
+      <div></div>
+    </StyledToggleSwitch>
+  );
+}
+
+const StyledToggleSwitch = styled.div`
+  width: 38px;
+  height: 18px;
+  padding: 1px;
+  background: "#FFFFFF";
+  border: 1px solid #cfcfcf;
+  border-radius: 9px;
+
+  div {
+    width: 14px;
+    height: 14px;
+    background: #2f80ed;
+    border-radius: 100%;
+  }
+
+  span {
+    font-size: 14px;
+    line-height: 17px;
+    color: #102c51;
+  }
+`;
